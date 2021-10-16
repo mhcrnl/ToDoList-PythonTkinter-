89677a6943ccbc3b7b420ac90bdf08ff346b2fc6
mhcrnl <mhcrnl@gmail.com>
2021-10-16 18:47:09 +0300

    sâmbătă 16 octombrie 2021, 18:47:09 +0300

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# --------------------------------------------------------
 from tkinter import *
 from tkinter import messagebox
+# --------------------------------------------------------
 
 
 class Window:
